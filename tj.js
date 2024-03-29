fb7910e3f656097df493ff45a806a22a2c248aa4
whuanle <1586052146@qq.com>
2024-03-29 08:44:35 +0800

    1

--- tj.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d06cb76fda572dae6307acadf95f2443";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
